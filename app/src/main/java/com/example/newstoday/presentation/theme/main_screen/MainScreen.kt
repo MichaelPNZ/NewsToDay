5a3fda4408935198bfd14a6d58594e36c8f64cc8
EvgeniyPoleshchuk <123636823+EvgeniyPoleshchuk@users.noreply.github.com>
2024-03-26 23:33:21 +0300

    добавил экран новости

--- app/src/main/java/com/example/newstoday/presentation/theme/main_screen/MainScreen.kt
+++ app/src/main/java/com/example/newstoday/presentation/theme/main_screen/MainScreen.kt
@@ -4,19 +4,27 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.newstoday.domain.model.Article
+import com.example.newstoday.domain.model.Source
 import com.example.newstoday.navigation.NavigationItem
 import com.example.newstoday.presentation.theme.account_screen.AccountScreen
 import com.example.newstoday.navigation.BottomNavigationBar
+import com.example.newstoday.navigation.NavigationObject
 import com.example.newstoday.presentation.theme.category_screen_tabBar.CategoryScreen
 import com.example.newstoday.presentation.theme.favorite_screen.FavoriteScreen
+import com.example.newstoday.presentation.theme.home_screen.DetailsNewsScreen
 import com.example.newstoday.presentation.theme.home_screen.HomeScreen
-import com.example.newstoday.presentation.theme.test.TestScreen
+import com.example.newstoday.presentation.theme.test.TestState
+import com.example.newstoday.presentation.theme.test.TestViewModel
 import com.example.newstoday.presentation.theme.ui.NewsToDayTheme
 
 @Composable
@@ -35,9 +43,14 @@ fun MainScreen() {
 
 @Composable
 fun Navigation(navController: NavHostController) {
+    val viewModel: TestViewModel = hiltViewModel()
+    val screenState by viewModel.screenState.collectAsStateWithLifecycle(TestState.Initial)
     NavHost(navController, startDestination = NavigationItem.Home.route) {
         composable(NavigationItem.Home.route) {
-            HomeScreen()
+            HomeScreen(screenState) {
+                navController.currentBackStackEntry?.savedStateHandle?.set("dd", it)
+                navController.navigate(NavigationObject.DetailScreen.route)
+            }
         }
         composable(NavigationItem.Category.route) {
             CategoryScreen()
@@ -47,6 +60,11 @@ fun Navigation(navController: NavHostController) {
         }
         composable(NavigationItem.Account.route) {
             AccountScreen()
+        }
+        composable(NavigationObject.DetailScreen.route) {
+            val article = navController.previousBackStackEntry?.savedStateHandle?.get<Article>("dd")
+                ?: Article(Source("", ""), "", "", "", "", "", "", "")
+            DetailsNewsScreen(article)
         }
     }
 }
