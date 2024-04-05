9d2955e972524a9af792faa0478222767117350e
Umaq <feratj01@gmail.com>
2024-04-05 22:58:20 +0300

    Делаю экран

--- app/src/main/java/com/example/newstoday/presentation/theme/personal_account_screen/LanguageScreen.kt
+++ app/src/main/java/com/example/newstoday/presentation/theme/personal_account_screen/LanguageScreen.kt
@@ -1,7 +1,9 @@
 package com.example.newstoday.presentation.theme.personal_account_screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
+@Preview(showBackground = true)
 @Composable
 fun LanguageScreen(){
 
